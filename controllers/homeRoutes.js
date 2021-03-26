151817fbcfd2224c732a41a4a2a64cba89adea99
Dylan Jacob <dylanjacob.re@gmail.com>
2021-03-26 13:41:46 -0400

    more work on routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,31 +1,67 @@
 const router = require('experss').Router();
-const { User, Pokemon } = require('../models');
+const { User, Pokemon, Collection } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all of the
-router.get('/', async (req, res) => {
+// // get all of the
+// router.get('/', async (req, res) => {
+//     try {
+
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
+
+// get one collection
+router.get('/collection/:id', withAuth, async (req, res) => {
     try {
+        const dbPokemonData = await Collection.findByPk(req.params.id, {
+            include: [
+                {
+                    model: Pokemon,
+                    attributes: [
+                        'id',
+                        'name',
+                        'level',
+                        'hp',
+                        'types',
+                        'attacks',
+                        'weaknesses',
+                        'resistances',
+                        'convertedRetreatCost',
+                        'set',
+                        'artist',
+                        'rarity',
+                        'nationalPokedexNumbers',
+                        'images',
+                        'collection_id',
+                    ],
+                },
+            ],
+        });
 
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// get one card
-router.get('/pokemon/:id', async (req, res) => {
-    try {
-        const pokemonData = await Pokemon.findByPk(req.params.id);
-
-        const pokemon = pokemonData.get({ plain: true });
-
-        res.render('pokemon', { pokemon, loggedIn: req.session.loggedIn });
+        const collection = dbPokemonData.get({ plain: true });
+        res.render('collection', { collection, loggedIn: req.session.loggedIn });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
 
-// get route for logging in
+// get one card
+router.get('/pokemon/:id', async (req, res) => {
+        try {
+            const dbPokemonData = await Pokemon.findByPk(req.params.id);
+
+            const pokemon = dbPokemonData.get({ plain: true });
+
+            res.render('pokemon', { pokemon, loggedIn: req.session.loggedIn });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    });
+
+// for login
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/profile');
