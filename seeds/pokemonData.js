bb1209545d5eedfaf90ffed2aa2f2e37f3131b19
Nate Kent <ngkent75@gmail.com>
2021-03-26 13:40:06 -0400

    worked out functions of pokemon npm package

--- seeds/pokemonData.js
+++ seeds/pokemonData.js
@@ -1,16 +1,48 @@
 const { Pokemon } = require('../models');
 const pokemon = require('pokemontcgsdk');
+const shuffle = require('lodash.shuffle');
 
 pokemon.configure({ apiKey: '80be9899-d5a3-48b0-bced-3f2974372f12' })
 
-const pokemonData = [
-  pokemon.card.all()
+const pokemonId = 'ex11-1';
+const pokemonName = 'Beedrill';
+// pokemon.card.all({ q: `name:${pokemonName}` })
+//     .then((cards) => {
+//         console.log(cards) // "Beedrill"
+// });
+
+
+
+pokemon.card.all({ q: `name:${pokemonName}` })
     .then((cards) => {
-      for (let i = 0; i < 2; i++)
-        console.log(cards[i])
+        console.log(cards.map(card => {
+          return {
+            name: card.name,
+            image: card.images.large,
+            rarity: card.rarity,
+            id: card.id,
+            // price: card.tcgplayer.prices.holofoil.market
+          }
+        })) 
+        
     })
-];
 
-const seedPokemon = () => Pokemon.bulkCreate(pokemonData);
+// const page = Math.ceil(Math.random()*10);
+// console.log(page);
+// pokemon.card.where({ pageSize:5, page })
+//     .then((cards) => {
+//         const randomData = shuffle(cards.data)
+//         console.log(randomData.map(card => {
+//           return {
+//             name: card.name,
+//             image: card.images.large,
+//             rarity: card.rarity,
+//             id: card.id,
+//             price: card.tcgplayer.prices.holofoil.market
+//           }
+//         })) // "Blastoise"
+//         // console.log(cards.data);
+//     })
 
-module.exports = seedPokemon;
+
+// module.exports = seedPokemon;
