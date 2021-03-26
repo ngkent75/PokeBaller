bee51c69c17001350dea1e4d85ef1dd28cd8f769
Nate Kent <ngkent75@gmail.com>
2021-03-26 16:35:12 -0400

    worked on routes

--- controllers/api/pokeRoutes.js
+++ controllers/api/pokeRoutes.js
@@ -1,3 +1,57 @@
 const router = require('express').Router();
-const { Pokemon } = require('../../models');
+const { Pokemon, User, PokemonUser } = require('../../models');
+
+
+
+// const pokemonName = 'Beedrill';
+
+const pokemonName = 'Beedrill'
+
+const findAllPokemonByName = (name) => {pokemon.card.all({ q: `name:${name}` })
+    .then((cards) => {
+        return(cards.map(card => {
+            return {
+                name: card.name,
+                image: card.images.large,
+                rarity: card.rarity,
+                id: card.id,
+            }
+        }))
+
+})};
+
+router.get('/', (req, res) => {
+    console.log(findAllPokemonByName(pokemonName));
+    try {
+        res.status(200).json(findAllPokemonByName(pokemonName))
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+const pokemonId = 'ex11-1';
+
+module.exports = router;
+
+// const pokemonGet = () => {pokemon.card.all({ q: `id:${pokemonId}` })
+// .then((cards) => {
+//     console.log(cards.map(card => {
+//         return {
+//             name: card.name,
+//             image: card.images.large,
+//             rarity: card.rarity,
+//             id: card.id,
+//         }
+//     })) 
+    
+// })
+// }
+// router.get('/:id', async (req, res) => {
+//     try {
+//         res.status(200).json(pokemonGet)
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// })
+
 
