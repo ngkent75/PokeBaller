7c769896089722f24b14b646218cbcc255ba838d
Dylan Jacob <dylanjacob.re@gmail.com>
2021-03-26 11:15:51 -0400

    more changes to the controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('experss').Router();
 const { User, Pokemon } = require('../models');
 const withAuth = require('../utils/auth');
 
+// get all of the
 router.get('/', async (req, res) => {
     try {
 
@@ -10,14 +11,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+// get one card
 router.get('/pokemon/:id', async (req, res) => {
     try {
+        const pokemonData = await Pokemon.findByPk(req.params.id);
 
+        const pokemon = pokemonData.get({ plain: true });
+
+        res.render('pokemon', { pokemon, loggedIn: req.session.loggedIn });
     } catch (err) {
+        console.log(err);
         res.status(500).json(err);
     }
 });
 
+// get route for logging in
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
         res.redirect('/profile');
