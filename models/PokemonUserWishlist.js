651ee49761894ce1d00598827704738958866373
James Jordan <jpjordan1987@gmail.com>
2021-05-18 14:40:55 -0400

    PokemonUserWishlist added model

---
+++ models/PokemonUserWishlist.js
@@ -0,0 +1,41 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class PokemonUserWishlist extends Model { }
+
+PokemonUserWishlist.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        pokemon_id: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'pokemon',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'pokemon_user_wishlist',
+    }
+);
+
+
+
+module.exports = PokemonUserWishlist;
